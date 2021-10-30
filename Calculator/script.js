f798f9c4aac9b247fb5c39cba83da10795ea3493
asobirov <asobirov357@gmail.com>
2021-10-30 21:26:47 +0100

    Added comments, number base converstion + Minor fixes

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,21 +1,29 @@
 
 
-
-const buttons = [...document.getElementsByClassName('calc-pad')[0].children]
-
+/**
+ * Buttons list from the children of div.calc-pad
+ */
+const buttons = [...document.getElementsByClassName('calc-pad')[0].children].map(c => [...c.children]).flat()
+/**
+ * Adding click listeners to every button from the buttons list
+ */
 buttons.forEach(btn => {
     btn.addEventListener("click", handleBtn)
 })
 
-document.addEventListener('keyup', handleBtn);
-
-// current, previous, operation
+/**
+ * Calculator states: current, previous, operation
+ */
 let state = ['0', '0', null];
+
+/**
+ * Current global radix of the calculator
+ */
 let radix = 10;
-let isDirty = false;
 
-console.log(radix)
-
+/**
+ * Dictionary of special keys in the calculator
+ */
 const keys = {
     clean: '⟲',
     del: '⌫',
@@ -28,23 +36,21 @@ const keys = {
     eval: '＝',
     toBinary: 'X₂',
     toNormal: 'X₁₀',
-    toHex: 'X₁₆'
+    toHex: 'X₁₆',
 }
 
-function getByDataType(t) {
-    if (t instanceof Array) {
-        let elements = []
-        t.forEach(type => {
-            elements.push(document.querySelector(`[data-type="${type}"]`))
-        })
-    }
-}
-
+/**
+ * Function to handle radix change. Controls states of HTML elements.
+ * @param {number} r 
+ */
 function setRadix(r) {
     radix = r;
+    let hexNums = document.querySelectorAll("[data-radix='16']");
+    let nums = document.querySelectorAll("[data-radix='10']");
+    let binNums = document.querySelectorAll("[data-radix='2']");
+
     switch (r) {
         case 2:
-            console.log(document.querySelector('[data-type="binary"]'))
             document.querySelector('[data-type="binary"]').classList.add('active');
             document.querySelector('[data-type="decimal"]').classList.remove('active');
             document.querySelector('[data-type="hex"]').classList.remove('active');
@@ -61,27 +67,89 @@ function setRadix(r) {
             document.querySelector('[data-type="decimal"]').classList.remove('active');
             document.querySelector('[data-type="hex"]').classList.add('active');
             break;
+
+        default:
+            radix = 10;
+            document.querySelector('[data-type="binary"]').disabled = false;
+            document.querySelector('[data-type="decimal"]').disabled = true;
+            document.querySelector('[data-type="hex"]').disabled = false;
+            break;
     }
+
+    r >= 16 ? hexNums.forEach(n => n.disabled = false) : hexNums.forEach(n => n.disabled = true);
+    r >= 10 ? nums.forEach(n => n.disabled = false) : nums.forEach(n => n.disabled = true);
+    r >= 2 ? binNums.forEach(n => n.disabled = false) : binNums.forEach(n => n.disabled = true);
 }
 
-function format(num) {
-    const str = num.toString();
-    const ints = parseInt(str.split('.')[0], radix);
-    const decimals = str.split('.')[1];
+/**
+ * Alternitve to parseInt for numbers with floating point
+ * @returns {string} parsed number
+ */
+function parseFloat(string, radix) {
+    let res;
+    const [i, d] = string.split('.');
+    const iNum = parseInt(i, radix);
+    const sign = Infinity / iNum === Infinity ? 1 : -1
+    res = iNum
 
-    let res
-    if (!ints) {
-        res = '0';
-    } else {
-        res = ints.toLocaleString('en', { maximumFractionDigits: 0 }).replace(/,(?=[^\s])/g, " ")
+    if (d) {
+        const dNum = parseInt(d, radix);
+        const dLength = Math.max(dNum.toString(radix).length, d.length);
+        res = iNum + sign * dNum / radix ** dLength;
     }
-    return decimals ? `${res}.${decimals}` : res
 
+    return res;
 }
 
+/**
+ * Calculate from states
+ */
+function evaluate() {
+    switch (state[2]) {
+        case keys.plus:
+            state[0] = (parseFloat(state[1], radix) + parseFloat(state[0], radix)).toString(radix);
+            state[2] = keys.plus
+            break;
+
+        case keys.minus:
+            console.log(state[1], state[0])
+            state[0] = (parseFloat(state[1], radix) - parseFloat(state[0], radix)).toString(radix);
+            break;
+
+        case keys.multiplication:
+            state[0] = (parseFloat(state[1], radix) * parseFloat(state[0], radix)).toString(radix);
+            break;
+
+        case keys.division:
+            state[0] = (parseFloat(state[1], radix) / parseFloat(state[0], radix)).toString(radix);
+            break;
+    }
+    state[2] = null;
+    state[1] = '0';
+}
+
+/**
+ * Handles buttton clicks
+ * @param {Event} e 
+ */
+function handleBtn(e) {
+    const key = (e.type === 'click' ? e.target.textContent.trim() : e.key);
+
+    /**
+     * Checks if the key is either number or '.' or exists in the special keys dictionary
+     */
+    if (!isNaN(key) || Object.keys(keys).find(k => keys[k] === key) || '.') {
+        handleKey(key);
+    }
+    updateCalc();
+}
+
+/**
+ * Update calculator element values
+ */
 function updateCalc() {
     document.getElementsByClassName('calc-result')[0].textContent = format(state[0]);
-
+    console.log(state[0])
     if (state[2]) {
         document.getElementsByClassName('calc-history')[0].textContent = `${format(state[1])} ${state[2]}`
     } else {
@@ -89,62 +157,77 @@ function updateCalc() {
     }
 }
 
-function evaluate() {
-    switch (state[2]) {
-        case keys.plus:
-            state[0] = (parseInt(state[1], radix) + parseInt(state[0], radix)).toString(radix);
-            state[2] = keys.plus
-            break;
+/**
+ * Format number
+*/
+function format(num) {
+    const str = num.toString(radix);
 
-        case keys.minus:
-            state[0] = (parseInt(state[1], radix) - parseInt(state[0], radix)).toString(radix);
-            break;
+    const ints = str.split('.')[0];
+    const decimals = str.split('.')[1];
 
-        case keys.multiplication:
-            state[0] = (parseInt(state[1], radix) * parseInt(state[0], radix)).toString(radix);
-            break;
-
-        case keys.division:
-            state[0] = (parseInt(state[1], radix) / parseInt(state[0], radix)).toString(radix);
-            break;
-    }
-    state[2] = null;
-    state[1] = '';
+    let res = ints ? ints.replace(/,(?=[^\s])/g, " ") : "0";
+    return decimals ? `${res}.${decimals}`.toUpperCase() : res.toUpperCase()
 }
 
-function handleBtn(e) {
-    const key = (e.type === 'click' ? e.target.textContent.trim() : e.key);
-
-    if (!isNaN(key) || Object.keys(keys).find(k => keys[k] === key)) {
-        isDirty = true
-        handleKey(key);
-    }
-    updateCalc();
+function lockdown() {
+    buttons.forEach(b => b.disabled = true);
+    document.querySelector('[data-button="reset"]').disabled = false;
 }
 
+function reset() {
+    state = ['0', '0', null]
+    buttons.forEach(b => b.disabled = false);
+    setRadix(radix);
+}
+/**
+ * Handles pressed key
+ * @param {*} key 
+ */
 function handleKey(key) {
     switch (key) {
-
+        /**
+         * Evaluate: calculate data from states
+         * @author Akbarshokh Sobirov
+         */
         case keys.eval:
             evaluate();
+            if (isNaN(state[0])) {
+                lockdown()
+            }
             break;
 
+        /**
+         * Handles all keys from keys Object
+         */
         case Object.keys(keys).find(k => keys[k] === key) && key:
             switch (key) {
+                /**
+                 * Clean: reset all states
+                 * @author Akbarshokh Sobirov
+                 */
                 case keys.clean:
-                    state = ['', '', null]
+                    reset();
                     break;
 
+                /**
+                 * Deletion: remove last character of the state[0]
+                 * @author Akbarshokh Sobirov
+                 */
                 case keys.del:
                     if (!!state[0]) {
                         state[0] = state[0].toString(radix).slice(0, -1)
                     } else {
-                        state[0] = ''
+                        state[0] = '0'
                     }
                     break;
 
+                /**
+                 * Subtraction
+                 * @author Akbarshokh Sobirov
+                 */
                 case keys.negate:
-                    const neg = -parseInt(state[0], radix);
+                    const neg = -parseFloat(state[0], radix);
                     if (neg) {
                         state[0] = neg;
                     }
@@ -154,132 +237,73 @@ function handleKey(key) {
                     state[0] = state[0] / 100
                     break;
 
+                /**
+                 * Function to convert to hexidecimal system
+                 * @author 
+                 */
                 case keys.toHex:
-
-                    state[0] = parseInt(state[0], radix).toString(16);
-                    state[1] = parseInt(state[1], radix).toString(16);
+                    state[0] = parseFloat(state[0], radix).toString(16);
+                    state[1] = parseFloat(state[1], radix).toString(16);
 
                     setRadix(16)
-                    console.log('New Hex Values', state[0], state[1]);
+                    updateCalc();
+
                     break;
 
+                /**
+                 * Function to convert to decimal system
+                 * @author
+                 */
                 case keys.toNormal:
-                    state[0] = parseInt(state[0], radix).toString(10);
-                    state[1] = parseInt(state[1], radix).toString(10);
+                    state[0] = parseFloat(state[0], radix).toString(10);
+                    state[1] = parseFloat(state[1], radix).toString(10);
 
                     setRadix(10)
-                    console.log('New Decimal Values', state[0], state[1]);
+                    updateCalc();
+
                     break;
 
+                /**
+                 * Function to convert to binary numeral system
+                 * @author
+                 */
                 case keys.toBinary:
-                    state[0] = parseInt(state[0], radix).toString(2);
-                    state[1] = parseInt(state[1], radix).toString(2);
+                    state[0] = parseFloat(state[0], radix).toString(2);
+                    state[1] = parseFloat(state[1], radix).toString(2);
 
-                    setRadix(2)
-                    console.log('New Binary Values', state[0], state[1]);
+                    setRadix(2);
+                    updateCalc();
+
                     break;
 
+                /**
+                 * Handles the state transfer
+                 * @author Akbarshokh Sobirov
+                 */
                 default:
-                    if (state[1]) {
-                        state[2] = key
-                    }
                     state[2] = key;
                     state[1] = state[0];
-                    state[0] = '';
+                    state[0] = '0';
             }
             break;
 
+        /**
+         * Used to add decimal point
+         */
+        case '.':
+            if (state[0].includes('.')) {
+                break;
+            }
+            state[0] += '.'
+            break;
+
+        /**
+         * Default function (case) to handle entered number
+         */
         default:
             if (key) {
-                if (key === '.' && state[0].includes('.')) {
-                    break;
-                }
-                state[0] = state[0].toString() + key.toString()
+                state[0] = parseFloat(state[0] + key, radix).toString(radix);
             }
+            break;
     }
 }
-// let calcHistory = '';
-// let calcResult = '';
-// let operation = null;
-
-
-// function reset() {
-//     calcHistory = '';
-//     calcResult = ''
-
-//     document.getElementsByClassName('calc-history')[0].textContent = '0'
-//     document.getElementsByClassName('calc-result')[0].textContent = '0'
-// }
-
-// function formatStr(i) {
-//     return parseFloat(i.trim()).toLocaleString('en').replace(/,(?=[^\s])/g, " ");
-// }
-
-// function numToStr(i) {
-//     return i.replace(/\s/g, '')
-// }
-
-// function setHtml() {
-//     document.getElementsByClassName('calc-history')[0].textContent = formatStr(calcHistory);
-//     document.getElementsByClassName('calc-result')[0].textContent = formatStr(calcResult)
-// }
-
-// function makeCalc(key) {
-//     operation = key;
-//     calcHistory += key
-//     setHtml()
-//     // document.getElementsByClassName('calc-result')[0].textContent = '0'
-// }
-
-// function handleBtn(e) {
-//     console.log(calcHistory, calcResult)
-//     const keys = {
-//         clean: 'C',
-//         del: '⟲',
-//         negate: '±',
-//         perc: '%',
-//         plus: '+',
-//         minus: '-',
-//         multiplication: '×',
-//         division: '／',
-//         eval: '＝',
-//         dot: '.'
-//     }
-
-//     const pressedKey = e.target.textContent.trim();
-
-//     switch (pressedKey) {
-//         case keys.eval:
-//             break;
-
-//         case Object.keys(keys).find(key => keys[key] === pressedKey) && pressedKey:
-
-//             switch (pressedKey) {
-//                 case keys.clean:
-//                     reset()
-//                     break;
-
-//                 case keys.del:
-//                     calcHistory = calcHistory.slice(0, -1)
-//                     calcResult = calcResult.slice(0, -1)
-//                     setHtml()
-//                     break;
-
-//                 default:
-//                     if (!operation || operation != pressedKey) {
-//                         makeCalc(pressedKey)
-//                     }
-
-
-//             }
-//             break;
-
-//         default:
-//             if (calcResult.length < 15) {
-//                 calcHistory += pressedKey;
-//                 calcResult += pressedKey;
-
-//                 setHtml();
-//             }
-//     }
-// }
